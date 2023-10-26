54c1b50886dc4236c9ca97c731342293f72e1acc
Nicole Liang <nliang@stu.naperville203.org>
2023-10-26 14:09:04 -0500

    Square branch completed

---
+++ src/SquareDemo.java
@@ -0,0 +1,8 @@
+public class SquareDemo {
+
+    public static void main(String[] args)
+    {
+        Square square = new Square(0, 3, 'E');
+        System.out.println(square.getRow() + " " + square.getCol() + " " + square.getType() + " " + square);
+    }
+}
