5c98796cceb17fd043246e2de54ad4e300579b59
Nicole Liang <111467644+NicoleLiang21@users.noreply.github.com>
2023-10-31 14:20:36 -0500

    myStack progress

---
+++ src/MyStack.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class MyStack
+{
+    // Tail of the ArrayList will be implemented as the 
+    ArrayList array = new ArrayList<>();
+
+    /*
+     * Constructor for stack class 
+     */
+    public MyStack()
+    {
+        
+    }
+
+    /*
+     * .add() - add an element to this array
+     * @param Object element
+     */
+
+    /*
+     * .get() - add an element to this array
+     * @param int index
+     */
+
+    /*
+     * .set() - set this index to hold this element
+     * @param int index, Object eleent
+     */
+
+    /*
+     * .remove() - remove the element at this index 
+     */
+}
